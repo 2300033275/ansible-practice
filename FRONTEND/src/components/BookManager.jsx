085511e8396fe5a365d9d3bede5cd57c427a7acc
SAI <2300033275@kluniversity.in>
2025-11-07 11:49:44 +0530

    updated

--- FRONTEND/src/components/BookManager.jsx
+++ FRONTEND/src/components/BookManager.jsx
@@ -1,10 +1,10 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 import "./style.css";
-import config from "./config.js";
+
 
 function BookManager() {
-  const baseUrl = `${config.url}/bookapi`;
+  const baseUrl = `${import.meta.env.VITE_API_URL}/bookapi`;
 
   const [books, setBooks] = useState([]);
   const [form, setForm] = useState({
